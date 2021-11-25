e5483fc612c4c521c5ce3bc9e18abad3b0753bf7
Anita Schiller <anita.s1@gmx.de>
2021-11-25 14:41:34 +0100

    Added solution for second variant of answer-toggle

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,22 @@
 const buttons = document.querySelectorAll('.card__button');
+
+// **Variante 1**
+
+/* 
 const answers = document.querySelectorAll('.card__answer');
 
 buttons.forEach((button, index) => {
   button.addEventListener('click', () => {
     answers[index].classList.toggle('hidden');
+  });
+}); 
+*/
+
+// **Variante 2**
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const answer = button.parentNode.querySelector('.card__answer');
+    answer.classList.toggle('hidden');
   });
 });
