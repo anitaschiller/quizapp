a83c909590c3dde9a891a8868e4b222cb9d4109b
Anita Schiller <anita.s1@gmx.de>
2021-11-25 14:26:31 +0100

    Removed js

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import { toggleAnswer, toggleBookmark } from './toggle.js';
-
-const buttons = document.querySelectorAll('.card__button');
-buttons.forEach(toggleAnswer);
-
-const bookmarks = document.querySelectorAll('.card__bookmark');
-bookmarks.forEach(toggleBookmark);
